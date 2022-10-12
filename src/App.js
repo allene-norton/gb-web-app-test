30fc091db5664ff9dcec3317a49e932657e60321
Allene Norton <allene@allenes-mbp.mynetworksettings.com>
2022-10-12 18:25:15 -0400

    user id obtained

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 
 function App() {
-  // let userId= window.App.state.user.id
-  // console.log(userId)
   console.log("I am the web app!")
-  console.log(window.location.search)
   let userId = window.location.search.slice(10)
   console.log('userId:', userId)
   return (
