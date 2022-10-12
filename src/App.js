46af7d4b1afa5e7da734d3406f2f6641a21ca4fe
Allene Norton <allene@allenes-mbp.mynetworksettings.com>
2022-10-12 18:14:02 -0400

    slice

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   // console.log(userId)
   console.log("I am the web app!")
   console.log(window.location.search)
+  let userId = window.location.search
+  console.log(userId.slice(10))
   return (
     <div className="App">
       <h1 className="header">GB test</h1>
