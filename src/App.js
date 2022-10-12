60f026053df1505bf91578f3b9292b605775b633
Allene Norton <allene@allenes-mbp.mynetworksettings.com>
2022-10-12 18:07:58 -0400

    test log

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   // let userId= window.App.state.user.id
   // console.log(userId)
   console.log("I am the web app!")
-  console.log(window.parent.location)
+  console.log(window.location)
   return (
     <div className="App">
       <h1 className="header">GB test</h1>
